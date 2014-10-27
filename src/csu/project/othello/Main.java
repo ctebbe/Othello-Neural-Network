9e75cc7ad4f0cd9d756f1965ceb8297b7d76a979
ct <calebtebbe@gmail.com>
2014-10-27 14:23:42 -0600

    added heaton library for managing the neural network

--- src/csu/project/othello/Main.java
+++ src/csu/project/othello/Main.java
@@ -1,7 +1,10 @@
 package csu.project.othello;
 
+import com.heatonresearch.book.introneuralnet.ch6.TicTacToe.NeuralTicTacToe;
+
 public class Main {
 
     public static void main(String[] args) {
+        NeuralTicTacToe nttt;
     }
 }
